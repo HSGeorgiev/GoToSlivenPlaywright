e1fbbee8918744b2a09edc0ae1201bd49e57178f
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-29 19:45:16 +0300

    BasePageProjectIdentification class added

---
+++ pages/BasePageProjectIdentification.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+export class BasePageProjectIdentification{
+    // BasePageProjectIdentification classs represent the mandatory visual identification 
+    constructor(page){
+        this.page = page;
+        this.path = '/';
+        // Text - Project Identification
+        this.projectIdentificationText = page.locator('//div[@data-id="45de748"]');
+        // Logos Elements
+        this.coatOfArmsSliven = page.locator('//img[contains(@src, "Лого-Сливен")]');
+        this.friendOfSlivenLogo = page.locator('//img[contains(@src, "Приятел-на-Сливен")]');
+        this.fundEFRRLogo = page.locator('//img[contains(@src, "Европейски_Фонд_за_регионално_Развитие")]');
+        this.fundFUGLogo = page.locator('//img[contains(@src, "Фонд_за_Устойчиви_Градове")]');
+        this.programOPRRLogo = page.locator('//img[contains(@src, "Региони_В_Растеж")]');
+        this.fundOfFundsLogo = page.locator('//img[contains(@src, "Фонд_на_Фондовете")]');
+
+       
+        
+    }
+
+}
+
