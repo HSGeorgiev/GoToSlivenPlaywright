3fa7cc8f0c3647781df1b0ce4df31180305edf14
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-04 14:40:07 +0300

    FrienOfSliven class added

---
+++ pages/FriendOfSlivenPage.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+export class TouFriendOfSlivenPageiFrienOfSlivenPagetAtractionsPage{
+    
+    constructor(page){
+
+        this.page = page;
+        this.path = '/tourist-attractions/';
+
+        // Slider
+        this.h2Elements = page.locator('//h2');
+        this.expectedH2Elements = [
+            'LANDMARKS',
+            'CULTURAL & HISTORICAL HERITAGE',
+            'PARKS & PLACES FOR ENTERTAINMENT',
+        ];
+        this.seeMoreButtons = page.locator('//span[@class="elementor-button-text"]');
+        this.expectedUrlTargets = [
+            '/landmarks-2/',
+            '/cultural-historical-heritage/',
+            '/parks-places-for-entertainment/',
+        ];
+
+        
+
+
+        
+    }
+
+}
+
--- pages/TouristAtractionsPage.js
+++ pages/TouristAtractionsPage.js
@@ -5,21 +5,8 @@ export class TouristAtractionsPage{
     constructor(page){
 
         this.page = page;
-        this.path = '/tourist-attractions/';
+        this.path = '/become-a-friend-of-sliven/';
 
-        // Slider
-        this.h2Elements = page.locator('//h2');
-        this.expectedH2Elements = [
-            'LANDMARKS',
-            'CULTURAL & HISTORICAL HERITAGE',
-            'PARKS & PLACES FOR ENTERTAINMENT',
-        ];
-        this.seeMoreButtons = page.locator('//span[@class="elementor-button-text"]');
-        this.expectedUrlTargets = [
-            '/landmarks-2/',
-            '/cultural-historical-heritage/',
-            '/parks-places-for-entertainment/',
-        ];
 
         
 
