b0dd7e1bac6877c2a911eac91f671d84b199e551
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-10 16:51:59 +0300

    Some objects mapped - VideoPage class and thair expected values discribed

--- pages/VideosPage.js
+++ pages/VideosPage.js
@@ -8,12 +8,29 @@ export class VideosPage{
         this.page = page;
         this.path = '/video/';
 
-        this.h3Headings = page.locator('//h3');
+        this.h3VideoHeadings = page.locator('//h3');
+        this.videoObjectsNames = page.locator('//div[@class="elementskit-box-style-content"]')
+        this.videoButtons = page.locator('//a[@class="elementskit-btn whitespace--normal"]')
         
+        this.h3Expected = [
+            'Historical Museum', 
+            'House-Museum', 
+            'House-Museum', 
+            'Fortress', 
+            'Textile Factory', 
+            'House-museum',
+        ]
 
-
-
+        this.videoNamesExpected = [
+            'D.r Simeon Tabakov', 
+            'Hadzhi Dimitar', 
+            'Old Sliven Popular Customs', 
+            'Tuida', 
+            'The First State Textile Factory In Balkan Peninsula', 
+            'Dobri Chintulov',
+        ]
     }
+
 
 }
 
