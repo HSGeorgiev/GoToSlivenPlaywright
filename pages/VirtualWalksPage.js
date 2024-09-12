396f4be15b6ddcf6e9d43aca0625acd3289de692
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-12 17:00:58 +0300

    VirtualWalksPage class added

---
+++ pages/VirtualWalksPage.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+export class VirtualWalksPage{
+    
+
+    constructor(page){
+
+        this.page = page;
+        this.path = '/360-virtual-walk/';
+
+  
+    }
+
+
+}
+
