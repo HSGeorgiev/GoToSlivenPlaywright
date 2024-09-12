3321fc70f4de73736a3f112b0feef0650ac6cd92
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-12 17:06:52 +0300

    VirtualWalksPageTests.spec class added

---
+++ tests/VirtualWalksPageTests.spec.js
@@ -0,0 +1,42 @@
+import { test, describe, beforeEach, afterEach, beforeAll, afterAll, expect } from '@playwright/test';
+import { chromium } from 'playwright';
+import { GoToSliven } from '../Globals/GlobalConstants.js';
+import { VirtualWalksPage } from '../pages/VirtualWalksPage.js';
+
+let browser;
+let context;
+let page;
+
+
+
+describe("Virtual Walks Page e2e tests", () => {
+    
+    beforeAll(async () => {
+        browser = await chromium.launch();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+        context = await browser.newContext();
+        page = await context.newPage();
+    });
+
+    afterEach(async () => {
+        await page.close();
+        await context.close();
+    });
+
+
+   
+
+
+
+      
+   
+
+   
+
+});
