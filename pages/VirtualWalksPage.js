930b34d2bdf5ee17805673382ce17ec627fada1a
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-13 15:21:11 +0300

    Public properties added to virtualWalkPage class - h2 headings of the objects

--- pages/VirtualWalksPage.js
+++ pages/VirtualWalksPage.js
@@ -8,6 +8,16 @@ export class VirtualWalksPage{
         this.page = page;
         this.path = '/360-virtual-walk/';
 
+        this.h2ObjectsHeadings = page.locator('//h2[@class="elementor-heading-title elementor-size-default"]');
+        this.h2ExpectedHeadings = [
+            'House - Museum Hadzhi Dimitar',
+            'Museum of Textyle Industry',
+            'TICC - Blue Stones',
+            'Sirak Skitnik Galery',
+            'Mirkovich Galery',
+            'Dobrovich Galery',
+        ];
+
   
     }
 
