5b8901b623cef35987deaa966e60f7d438417a4c
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-18 16:39:53 +0300

    3D Models Page Title test added

--- tests/modelsPageTests.spec.js
+++ tests/modelsPageTests.spec.js
@@ -30,5 +30,24 @@ describe("3D Models Page e2e tests", () => {
     });
 
 
-    
+    describe("Tourist Attractions Page Elements Tests", () => {
+
+        test('3D Models Page Title', async ({ page }) => {
+            // Verify if the page title is correct
+
+            const modelsPage = new ModelsPage(page);
+            const goToSliven = new GoToSliven();
+
+            await page.goto(goToSliven.baseUrl + modelsPage.path);
+            var title = await page.title();
+            expect(title).toBe("3D Models – gotosliven Touristic Info Site");
+        });
+
+       
+
+
+
+        
+
+    });
 });
