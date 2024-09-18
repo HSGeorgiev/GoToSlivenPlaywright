d3763b3067c4d1cd3a7272708dcfc5e5ed8f2333
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-18 16:36:36 +0300

    C:/Program Files/Git/modelsPageTests.spec.js class added to contains ModelsPage tests

---
+++ tests/modelsPageTests.spec.js
@@ -0,0 +1,34 @@
+import { test, describe, beforeEach, afterEach, beforeAll, afterAll, expect } from '@playwright/test';
+import { chromium } from 'playwright';
+import { GoToSliven } from '../Globals/GlobalConstants.js';
+import { ModelsPage } from '../pages/ModelsPage.js';
+
+let browser;
+let context;
+let page;
+
+
+
+describe("3D Models Page e2e tests", () => {
+    
+    beforeAll(async () => {
+        browser = await chromium.launch();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+        context = await browser.newContext();
+        page = await context.newPage();
+    });
+
+    afterEach(async () => {
+        await page.close();
+        await context.close();
+    });
+
+
+    
+});
